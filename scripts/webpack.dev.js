e9554287155b4ab958eacaffea8f9b597d51fbd5
Eling Pramuatmaja <elingpramuatmaja@gmail.com>
2021-05-18 21:14:18 +0800

    revert: refactor: move CSP plugin to common webpack config
    This reverts commit 9de196922d7a1f844fa72d9fb9024e1d819bf010.

--- scripts/webpack.dev.js
+++ scripts/webpack.dev.js
@@ -2,6 +2,7 @@ const path = require("path");
 require("dotenv").config({
   path: path.resolve(__dirname, "..", ".env.development"),
 });
+const CspHtmlWebpackPlugin = require("csp-html-webpack-plugin");
 const Dotenv = require("dotenv-webpack");
 
 module.exports = {
@@ -17,6 +18,24 @@ module.exports = {
   plugins: [
     new Dotenv({
       path: path.resolve(__dirname, "..", ".env.development"),
+    }),
+    new CspHtmlWebpackPlugin({
+      "default-src": "'self'",
+      "connect-src": ["'self'", process.env.API_URL],
+      "font-src": [
+        "'self'",
+        "https://fonts.gstatic.com/",
+        "https://cdnjs.cloudflare.com/",
+      ],
+      "img-src": ["'self'", "data:", "https://via.placeholder.com/"],
+      "script-src": ["'self'", "'unsafe-eval'"],
+      "style-src": [
+        "'self'",
+        "'unsafe-inline'",
+        "https://fonts.googleapis.com/",
+        "https://cdnjs.cloudflare.com/",
+      ],
+      "form-action": "'self'",
     }),
   ],
   devServer: {
